11e6a89138726eb05d91b108f5f7cc25c874a87f
surya <suryanani2017@gmail.com>
2026-08-17 17:05:33 +0530

    ml_model.py

--- Backend/ml_model.py
+++ Backend/ml_model.py
@@ -1,16 +1,20 @@
 import joblib
 from pathlib import Path
 from huggingface_hub import hf_hub_download
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 MODEL_DIR = BASE_DIR / "Models"
 
+# Download large model to Vercel's writable /tmp directory
 model_path = hf_hub_download(
     repo_id="kanithisurya/ai-realestate-house-price-model",
-    filename="house_price_model.pkl"
+    filename="house_price_model.pkl",
+    cache_dir="/tmp/huggingface"
 )
 
+# Load model
 model = joblib.load(model_path)
 
+# Load smaller files from the project
 scaler = joblib.load(MODEL_DIR / "scaler.pkl")
 encoders = joblib.load(MODEL_DIR / "encoders.pkl")
