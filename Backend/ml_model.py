ebe6739d9b79d5f3c51b0192a3b3bca8cd8bbc99
surya <suryanani2017@gmail.com>
2026-08-17 16:54:46 +0530

    ml_model.py

--- Backend/ml_model.py
+++ Backend/ml_model.py
@@ -1,9 +1,16 @@
 import joblib
 from pathlib import Path
-
+from huggingface_hub import hf_hub_download
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 MODEL_DIR = BASE_DIR / "Models"
 
-model = joblib.load(MODEL_DIR / "house_price_model.pkl")
+model_path = hf_hub_download(
+    repo_id="YOUR_USERNAME/ai-realestate-house-price-model",
+    filename="house_price_model.pkl"
+)
+
+model = joblib.load(model_path)
+
 scaler = joblib.load(MODEL_DIR / "scaler.pkl")
 encoders = joblib.load(MODEL_DIR / "encoders.pkl")
