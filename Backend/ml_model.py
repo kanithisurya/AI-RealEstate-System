9211583a7c631e915a78f2c099ce4abbb1fdb50d
surya <suryanani2017@gmail.com>
2026-08-17 16:55:41 +0530

    ml_model.py

--- Backend/ml_model.py
+++ Backend/ml_model.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MODEL_DIR = BASE_DIR / "Models"
 
 model_path = hf_hub_download(
-    repo_id="YOUR_USERNAME/ai-realestate-house-price-model",
+    repo_id="kanithisurya/ai-realestate-house-price-model",
     filename="house_price_model.pkl"
 )
 
