ae8a39496d4e1bafc0445adb6e0b337f80c27ee4
surya <suryanani2017@gmail.com>
2026-08-18 12:24:42 +0530

    database.py

--- Backend/database.py
+++ Backend/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://postgres:Surya%402004@localhost:5432/real_estate_db"
+DATABASE_URL = "postgresql://postgres:@localhost:5432/real_estate_db"
 
 engine = create_engine(DATABASE_URL)
